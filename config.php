f9dd8c5269a96f65562bbe8fc2f06240ce103f25
audit system <other8822personne@gmail.com>
2026-03-21 23:30:07 +0000

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,14 @@
 // config.php — Connexion MySQL
 // Modifier les 4 lignes ci-dessous
 // ================================================
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'lpa_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');          // XAMPP = vide par défaut
+//define('DB_HOST', 'localhost');
+//define('DB_NAME', 'lpa_db');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');          // XAMPP = vide par défaut
+define('DB_HOST', 'sql111.infinityfree.com');
+define('DB_NAME', 'if0_41408771_lpa_db');
+define('DB_USER', 'if0_41408771');
+define('DB_PASS', '12CHAKIR');          // XAMPP = vide par défaut
 
 function db() {
     static $pdo = null;
