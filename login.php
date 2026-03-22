8a4ca0841324596e87cf1f9a5fbda46bf5e7e585
audit system <other8822personne@gmail.com>
2026-03-22 00:33:28 +0000

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,15 @@ session_start();
 require_once 'config.php';
 header('Content-Type: application/json; charset=utf-8');
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 if (isset($_GET['logout'])) {
     session_destroy();
     echo json_encode(['ok' => true]);
