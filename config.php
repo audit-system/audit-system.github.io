aae3fbaae3bd7c14c8359384ad2d8e936bd351ca
audit system <other8822personne@gmail.com>
2026-03-22 00:26:39 +0000

    Update config.php

--- config.php
+++ config.php
@@ -7,11 +7,16 @@
 //define('DB_NAME', 'lpa_db');
 //define('DB_USER', 'root');
 //define('DB_PASS', '');          // XAMPP = vide par défaut
-define('DB_HOST', 'sql.freedb.tech');
-define('DB_NAME', 'freedb_audit_db');
-define('DB_USER', 'freedb_auditdb_user');
-define('DB_PASS', 'avvZVJ@Ab2F3na&');          // XAMPP = vide par défaut
+//define('DB_HOST', 'sql.freedb.tech');
+//define('DB_NAME', 'freedb_audit_db');
+//define('DB_USER', 'freedb_auditdb_user');
+//define('DB_PASS', 'avvZVJ@Ab2F3na&');          // XAMPP = vide par défaut
 
+define('DB_HOST', 'db.fr-pari1.bengt.wasmernet.com');
+define('DB_NAME', 'dbHZLNNdXPdhYjLMBKaYs3zY');
+define('DB_USER', 'f3562a7b76b98000c108b810145f');
+define('DB_PASS', '069bf356-2a7b-77c2-8000-abe2733fecfc');          // XAMPP = vide par défaut
+   
 function db() {
     static $pdo = null;
     if ($pdo) return $pdo;
